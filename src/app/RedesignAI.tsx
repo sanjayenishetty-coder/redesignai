7abe3380e6ae11e462b8232f540e0e18c014bf81
sanjay Enishetty <sanjayenishetty@gmail.com>
2026-03-31 22:59:29 +0530

    Remove phone number, update email to sme@scaleme.in in footer

--- src/app/RedesignAI.tsx
+++ src/app/RedesignAI.tsx
@@ -696,11 +696,7 @@ export default function RedesignAI() {
               <ul className="footer-col-items">
                 <li>
                   <svg width="14" height="14" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>
-                  hello@redesignworkshop.in
-                </li>
-                <li>
-                  <svg width="14" height="14" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 11.3 19.79 19.79 0 0 1 1.61 2.68 2 2 0 0 1 3.59.5h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.05a16 16 0 0 0 6.06 6.06l.88-.88a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 21.5 15.5z"/></svg>
-                  +91 98765 43210
+                  sme@scaleme.in
                 </li>
               </ul>
             </div>
